dd84738f4e385c66eb7cede4e132b1020750d6a6
Arash7447 <arash.sotoudeh6@gmail.com>
2024-03-05 19:32:45 +0330

    created a general template folder
    in this part i created a general template folder for project and moved html files into it .

--- mysite/settings.py
+++ mysite/settings.py
@@ -52,10 +52,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'mysite.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
