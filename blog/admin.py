163fa86d570d950c0a2210f680ddd3b1dc602175
Arash7447 <arash.sotoudeh6@gmail.com>
2024-03-16 14:34:44 +0330

    actived logo link and author part
    actived logo link and author part

--- blog/admin.py
+++ blog/admin.py
@@ -8,8 +8,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_date"
     #empty_value_display = "-empty-"
-    list_display = ('title','counted_view','status','published_date')
-    list_filter = ('status',)
+    list_display = ('title','author','counted_view','status','published_date')
+    list_filter = ('status','author')
     #ordering = ['-created_date']
     search_fields = ['title','content']
 
