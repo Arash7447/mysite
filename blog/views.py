b661e2b6587da0bdae3a04cd70b0649de7de1b31
Arash7447 <arash.sotoudeh6@gmail.com>
2024-03-16 12:34:56 +0330

    .
    .

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from blog.models import Post
 from django.utils import timezone
 
@@ -11,9 +11,18 @@ def blog_view(request) :
 
 def blog_single(request,pid) :
     post = get_object_or_404(Post, pk=pid)
-    context = {'post': post}
+    context = context = {'post': post, 'counted_view': post.counted_view}
     post.counted_view += 1
     post.save()
-    return render(request, "blog/blog-single.html", context)
+    
+    if post.published_date <= timezone.now() :
+        return render(request, "blog/blog-single.html", context)
+    
+    else :
+        return redirect("blog_view")
+    
+    
+    
+    #return render(request, "blog/blog-single.html", context)
 
     
