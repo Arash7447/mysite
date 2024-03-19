1aab28597e4c3b7a9f25ae599c304300c1d0feab
Arash7447 <arash.sotoudeh6@gmail.com>
2024-03-19 17:56:43 +0330

    some parts are changed
    some parts are changed for popular post and template tag.

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,26 @@
+from django import template
+from blog.models import Post
+register = template.Library()
+
+@register.simple_tag(name = 'totalposts')
+
+def function () :
+    posts = Post.objects.filter(status = 1).count()
+    return posts 
+
+
+@register.simple_tag(name = 'posts')
+
+def function () :
+    posts = Post.objects.filter(status = 1)
+    return posts 
+
+@register.filter
+
+def snippet (value,arg = 20) :
+    return value [:arg] + '...'
+
+@register.inclusion_tag('blog/popular-posts.html')
+def latestposts(arg = 2) :
+    posts = Post.objects.filter(status = 1).order_by('published_date')[:arg]
+    return {'posts': posts}
