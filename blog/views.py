8df68f8b3e735248a883226d9addac3728ad6894
Arash7447 <arash.sotoudeh6@gmail.com>
2024-03-26 12:48:28 +0330

    paginator updated.
    in this part we worked on paginator.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.utils import timezone
-
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 # Create your views here.
 
 def blog_view(request, **kwargs) :
@@ -10,8 +10,24 @@ def blog_view(request, **kwargs) :
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username') != None:
         posts = posts.filter(author__username = kwargs['author_username'])
+    
+    posts = Paginator (posts,3)
+    try:
+        page_number = request.GET.get ('page')
+        posts = posts.get_page(page_number)
+
+    except PageNotAnInteger:
+        
+        posts = posts.get_page(1)
+
+    except EmptyPage:
+
+        posts = posts.get_page(1)
+
     context = {'posts': posts}
     return render(request, "blog/blog-home.html", context)
+
+
 
 def blog_single(request, pid) :
     post = get_object_or_404(Post, pk=pid, status=1, published_date__lte=timezone.now())
@@ -22,11 +38,17 @@ def blog_single(request, pid) :
     post.save()
     return render(request, "blog/blog-single.html", context)
 
+
+
+
 def blog_category(request, cat_name) :
     posts = Post.objects.filter(status = 1)
     posts = posts.filter(category__name=cat_name)
     context = {'posts': posts} 
     return render(request, "blog/blog-home.html",context)
+
+
+
 
 def blog_search(request):
     posts = Post.objects.filter(status=1)
