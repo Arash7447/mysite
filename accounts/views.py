bf8b4448a922e13a304eb027d9e909d8e6548718
Arash7447 <arash.sotoudeh6@gmail.com>
2024-04-05 17:39:00 +0330

    worked on sign up page
    worked on sign up page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse
 
 # Create your views here.
 
@@ -23,4 +24,11 @@ def logout_view(request) :
 
 
 def signup_view(request) :
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST' :
+        form = UserCreationForm(request.POST)
+        if form.is_valid() :
+            form.save()
+            return redirect('/')
+    form = UserCreationForm ()
+    context = {'form': form}
+    return render(request, 'accounts/signup.html', context)
